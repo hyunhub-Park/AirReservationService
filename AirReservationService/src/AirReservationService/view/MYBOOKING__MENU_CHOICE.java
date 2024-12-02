9c55dbb6708f15b8ee7bbfa83f8bded41b1fb777
hyunhub <nuyhos_ambi@naver.com>
2024-12-02 12:13:55 +0900

    [create]AirReservationService.view 인터페이스 등 업로드

---
+++ AirReservationService/src/AirReservationService/view/MYBOOKING__MENU_CHOICE.java
@@ -0,0 +1,8 @@
+package AirReservationService.view;
+
+public interface MYBOOKING__MENU_CHOICE {
+	int CANCLE = 1;
+	int UPDATE = 2;
+	int END = 3;
+
+}
---
+++ AirReservationService/src/AirReservationService/view/MYPAGE_MENU_CHOICE.java
@@ -0,0 +1,8 @@
+package AirReservationService.view;
+
+public interface MYPAGE_MENU_CHOICE
+{
+	int INFO_UPDATE = 1,
+		INFO_DELETE = 2,
+		END = 3;
+}
